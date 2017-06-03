72fc669f5388184d8157dbc201658254762420f8
ken1flan <ken1flan@gmail.com>
2017-06-03 21:42:34 +0900

    Add omniauth-google-oauth2

--- Gemfile
+++ Gemfile
@@ -21,6 +21,7 @@ gem 'jquery-rails'
 gem 'faml'
 gem 'haml-rails'
 gem 'bootstrap', git: 'https://github.com/twbs/bootstrap-rubygem'
+gem 'omniauth-google-oauth2'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170601223552) do
+ActiveRecord::Schema.define(version: 20170603123238) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title", null: false
     t.text "body", null: false
     t.integer "author_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "identities", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "provider", limit: 64, null: false
+    t.string "uid", limit: 64, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
