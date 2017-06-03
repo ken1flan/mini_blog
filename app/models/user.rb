784f97d4e6ea5c7e83279954b6c2aedfcc88fe58
ken1flan <ken1flan@gmail.com>
2017-06-03 22:25:30 +0900

    Fix user

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,8 @@
 class User < ApplicationRecord
   has_many :identities
   has_many :blogs
+
+  validates :nickname, presence: true, length: {maximum: 64}
+  validates :icon_url, length: {maximum: 255}
+  validates :introduction, length: {maximum: 255}
 end
