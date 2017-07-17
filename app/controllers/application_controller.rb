e54451f3f4fe6e6021946e1cf447f8c196151b83
ken1flan <ken1flan@gmail.com>
2017-07-17 13:49:58 +0900

    Add markdown_to_html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :user_signed_in?, :current_user
+  helper_method :user_signed_in?, :current_user, :markdown_to_html
 
   def not_found
     raise ActionController::RoutingError.new('Not Found')
@@ -13,5 +13,13 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def markdown_to_html(markdown_text)
+    @markdown ||= Redcarpet::Markdown.new(
+      Redcarpet::Render::HTML.new(hard_wrap: true),
+      autolink: true
+    )
+    @markdown.render(markdown_text).html_safe
   end
 end
