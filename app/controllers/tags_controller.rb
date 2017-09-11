4a33c0cf1dce0d863ad6e9840cc358a6280bdd27
ken1flan <ken1flan@gmail.com>
2017-09-12 07:00:08 +0900

    Add tag pages

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,10 @@
+class TagsController < ApplicationController
+  def index
+    @tags = Tag.all
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+    @blogs = @tag.blogs.published.normal_order
+  end
+end
