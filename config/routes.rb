2ec40a6bacf3bbe8247efa750d2423b2dfc64fad
ken1flan <ken1flan@gmail.com>
2018-06-16 16:15:33 +0900

    Fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :blogs
+  resources :users, only: %i[index show edit update destroy] do
+    resources :blogs, only: [:index], module: :users
+  end
+  resources :tags, only: %i[index show]
+
+  get '/sign_up', to: 'sign_up#index'
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/signout', to: 'sessions#destroy', as: :signout
+
+  root 'blogs#index'
 end
