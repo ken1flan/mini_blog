048601e95aa1b790e54531e730681728a88f631e
ken1flan <ken1flan@gmail.com>
2019-10-08 22:51:40 +0900

    Add system spec

---
+++ spec/system/my_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'My', type: :system do
+  context 'When user does not signed in' do
+    it 'displays warning' do
+      visit my_index_path
+
+      expect(page.text).to have_text('You must be logged in to access this section.')
+    end
+  end
+
+  context 'When user signed in' do
+    let(:user) { create(:user) }
+
+    before do
+      sign_in(user)
+    end
+
+    it 'displays my page' do
+      visit my_index_path
+
+      expect(page.text).to have_text('My page')
+    end
+  end
+end
